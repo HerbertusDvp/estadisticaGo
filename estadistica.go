ffa0686e7407bfae9cdead2bf0ee67cd90debade
HerbertusDvp <HerbertusDvp@gmail.com>
2024-06-30 15:30:16 -0600

    Feat: Addes MediaSlice function

--- estadistica.go
+++ estadistica.go
@@ -4,13 +4,22 @@ func MediaDatos(datos ...float64) float64 {
 	if len(datos) == 0 {
 		return 0
 	}
+	suma := 0.0
+	for _, dato := range datos {
+		suma += dato
+	}
+	return suma / float64(len(datos))
+}
 
+func MediaSlice(datos []float64) float64 {
+
+	if len(datos) == 0 {
+		return 0
+	}
 	suma := 0.0
 
 	for _, dato := range datos {
 		suma += dato
 	}
-
 	return suma / float64(len(datos))
-
 }
