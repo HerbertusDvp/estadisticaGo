50893a6bac6af5c99a020395ba7471bd73f91c8f
HerbertusDvp <HerbertusDvp@gmail.com>
2024-06-29 23:34:32 -0600

    Feat: confirmacion de archivos borrados

--- Main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Mi progrma principal")
-}
--- Utils.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func developing() {
-	fmt.Print("Utilidades en proceso")
-}
